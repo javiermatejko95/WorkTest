cc066d30a44e6ab0537a3a1a3b4aace985e69570
Javier Matejko <javiermatejko@pathfindersstudio.com>
2022-04-04 09:58:09 -0300

    Added RockSpawner logic.

--- WorkTest/Assets/Scripts/Thrower/RockSpawner.cs
+++ WorkTest/Assets/Scripts/Thrower/RockSpawner.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class RockSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    #region EXPOSED_FIELDS
+    [SerializeField] private GameObject rockPrefab = null;
+    [SerializeField] private Transform spawnPosition = null;
+    [SerializeField] private Transform floorPosition = null;
+    #endregion
 
-    // Update is called once per frame
-    void Update()
+    #region PRIVATE_FIELDS
+    private float interval = 1f;
+    private float currentTimer = 0f;
+    private bool on = false;
+    #endregion
+
+    #region UNITY_CALLS
+    private void Update()
     {
-        
+        if(currentTimer >= interval)
+        {
+            SpawnRock();
+            currentTimer = 0f;
+        }
+        else
+        {
+            currentTimer += Time.deltaTime;
+        }
     }
+    #endregion
+
+    #region PUBLIC_METHODS
+
+    #endregion
+
+    #region PRIVATE_METHODS
+    private void SpawnRock()
+    {
+        GameObject go = Instantiate(rockPrefab, spawnPosition);
+        go.transform.position = this.transform.position;
+        RockMovement rock = go.GetComponent<RockMovement>();
+        rock.Init(floorPosition);
+        rock.StartMoving();
+    }
+    #endregion
 }
