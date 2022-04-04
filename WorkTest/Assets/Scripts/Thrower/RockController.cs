b773389d375f1aae040113510449e3069330ce01
Javier Matejko <javiermatejko@pathfindersstudio.com>
2022-04-04 09:58:43 -0300

    Added RockController script.

---
+++ WorkTest/Assets/Scripts/Thrower/RockController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockController : MonoBehaviour
+{
+    #region EXPOSED_FIELDS
+
+    #endregion
+
+    #region PRIVATE_FIELDS
+
+    #endregion
+
+    #region UNITY_CALLS
+    private void Update()
+    {
+
+    }
+    #endregion
+
+    #region PUBLIC_METHODS
+
+    #endregion
+
+    #region PRIVATE_METHODS
+
+    #endregion
+}
